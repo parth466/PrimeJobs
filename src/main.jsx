28eac3b03b5722f4fc5795a4a8fdb1b756bffde8
parth466 <131953152+parth466@users.noreply.github.com>
2025-05-06 12:51:32 +0530

    adding render

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import Footer from './pages/Footer'
 import { ThemeProvider } from "@/components/themeprovider"
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/PrimeJobs">
    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
   <StrictMode>
     <Navbar/>
