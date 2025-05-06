d0f9cfe2010665b531687e4c67799b29ab2a96b3
parth466 <131953152+parth466@users.noreply.github.com>
2025-05-06 13:47:02 +0530

    adding versel

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import Footer from './pages/Footer'
 import { ThemeProvider } from "@/components/themeprovider"
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/PrimeJobs">
+  <BrowserRouter>
    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
   <StrictMode>
     <Navbar/>
