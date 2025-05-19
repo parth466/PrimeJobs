0bc817d029a2ae3e1e29218316df8e1d18971a6f
parth466 <131953152+parth466@users.noreply.github.com>
2025-05-19 15:21:21 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path"
 import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
- 
+ base: "/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
